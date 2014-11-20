320ad084d60a3f2ee6035cbc6c57d223a93e8350
Alexandre Perier-Muzet <andersflex69@gmail.com>
2014-11-20 11:23:57 +0100

    2nd
    Add description to code + link to readme

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 LisiParoi
 =========
 
-Code for the LisiParoi board
+Very simple code for the LisiParoi board : http://www.lisiparoi.com/
+
+More informartion : http://anderson69s.com/2014/11/20/lisiparoi-raspberry/
---
+++ on.py
@@ -0,0 +1,4 @@
+import RPi.GPIO as GPIO #On importe la librarie RPi.GPIO
+GPIO.setmode(GPIO.BOARD) #On définit l'utilisation des numéros sur le modèle BOARD et non BCM
+GPIO.setup(11, GPIO.OUT) #On définit le port 11 en sortie
+GPIO.output(11, GPIO.HIGH) #On mets le port 11 à l'état haut pour allumer les leds
