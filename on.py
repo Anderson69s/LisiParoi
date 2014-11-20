9269cea45cbcfdeb8205ba7bc00f1f705f72429b
Alexandre Perier-Muzet <andersflex69@gmail.com>
2014-11-20 11:43:48 +0100

    7th
    it this the one?

--- on.py
+++ on.py
@@ -1,8 +1,8 @@
 import RPi.GPIO as GPIO
 #On importe la librarie RPi.GPIO
 GPIO.setmode(GPIO.BOARD)
-#On définit l'utilisation des numeros sur le modele BOARD et non BCM
+#On definit l utilisation des numeros sur le modele BOARD et non BCM
 GPIO.setup(11, GPIO.OUT)
-#On définit le port 11 en sortie
+#On definit le port 11 en sortie
 GPIO.output(11, GPIO.HIGH)
-#On mets le port 11 à l'état haut pour allumer les leds
+#On mets le port 11 a l etat haut pour allumer les leds
