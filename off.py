e902e26c2ffe6a51c90898db5fd85ba24c98c325
Alexandre Perier-Muzet <andersflex69@gmail.com>
2014-11-20 11:31:16 +0100

    4th
    silly mistakes

--- off.py
+++ off.py
@@ -1,8 +1,8 @@
-import RPi.GPIO as GPIO 
+import RPi.GPIO as GPIO
 #On importe la librarie RPi.GPIO
-GPIO.setmode(GPIO.BOARD) 
+GPIO.setmode(GPIO.BOARD)
 #On définit l'utilisation des numéros sur le modèle BOARD et non BCM
-GPIO.setup(11, GPIO.OUT) 
+GPIO.setup(11, GPIO.OUT)
 #On définit le port 11 en sortie
-GPIO.output(11, GPIO.LOW) 
+GPIO.output(11, GPIO.LOW)
 #On mets le port 11 à l'état bas pour éteindre les leds
