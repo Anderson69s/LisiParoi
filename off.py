490ca26193a6994585aa7942d9fde15ff100aeb6
Alexandre Perier-Muzet <andersflex69@gmail.com>
2014-11-20 11:28:38 +0100

    3rd
    test

--- off.py
+++ off.py
@@ -1,4 +1,8 @@
-import RPi.GPIO as GPIO #On importe la librarie RPi.GPIO
-GPIO.setmode(GPIO.BOARD) #On définit l'utilisation des numéros sur le modèle BOARD et non BCM
-GPIO.setup(11, GPIO.OUT) #On définit le port 11 en sortie
-GPIO.output(11, GPIO.LOW) #On mets le port 11 à l'état bas pour éteindre les leds
+import RPi.GPIO as GPIO 
+#On importe la librarie RPi.GPIO
+GPIO.setmode(GPIO.BOARD) 
+#On définit l'utilisation des numéros sur le modèle BOARD et non BCM
+GPIO.setup(11, GPIO.OUT) 
+#On définit le port 11 en sortie
+GPIO.output(11, GPIO.LOW) 
+#On mets le port 11 à l'état bas pour éteindre les leds
